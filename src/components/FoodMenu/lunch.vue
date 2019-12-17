7761c211fde1a955d8670953eff6b83f1c9f4ace
NakamuraHiroki2465 <nakamura2465@gmail.com>
2019-12-17 17:06:19 +0900

    茶屋ページの作成

--- src/components/FoodMenu/lunch.vue
+++ src/components/FoodMenu/lunch.vue
@@ -2,17 +2,50 @@
     <div>
         <detail :datas="datas" :header="header"></detail>
         <subdetail :subdatas="subdatas"></subdetail>
-        <access></access>
         <lunchslideshow></lunchslideshow>
+        <menudetails :mains="mains"></menudetails>
+        <div class="wrapper60 menu-wrapper columns  is-multiline">
+            <table class="menu column is-5 is-offset-1 ">
+                <tr>
+                    <th>
+                        お料理
+                    </th>
+                    <th>
+                        料金
+                    </th>
+                </tr>
+                <tr v-for="menu in this.menus" :key="menu.name">
+                    <td>{{menu.name}}</td>
+                    <td>{{menu.price}}&nbsp;円</td>
+                </tr>
+            </table>
+            <table class="menu column is-5 is-offset-1 ">
+                <tr>
+                    <th>
+                        ドリンク
+                    </th>
+                    <th>
+                        料金
+                    </th>
+                </tr>
+                <tr v-for="drink in this.drinks" :key="drink.name">
+                    <td>{{drink.name}}</td>
+                    <td>{{drink.price}}&nbsp;円</td>
+                </tr>
+            </table>
+        </div>
+        <access></access>
     </div>
 </template>
 <script>
 import access from '@/components/Parts/access.vue'
 import lunchslideshow from '@/components/Parts/lunchslideshow.vue'
+import menudetails from '@/components/Parts/menudetails.vue'
 export default {
     components:{
         access,
-        lunchslideshow
+        lunchslideshow,
+        menudetails
     },
     data:function(){
         return{
@@ -35,8 +68,177 @@ export default {
                     img:require('@/assets/images/shiro-dango1.jpeg'),
                     imgtitle:'太平山名物・だんご'
                 }
+            ],
+            mains:[
+                {
+                    name:'だんご',
+                    price:'300',
+                    text:'太平山の代表的な名物であるだんごを上新粉と水のみで仕上げています。添加物や無駄なものを一切使っておりませんので、日持ちはいたしませんが、上質で味わい深いだんごをお楽しみいただけます。',
+                    relatemenu:false,
+                    img:require('@/assets/images/dango-pic.png'),
+                    alt:'だんごイラスト'
+                },
+                {
+                    name:'玉子焼',
+                    price:'350',
+                    text:'門外不出の出汁を使用したふわふわであまじょっぱい風味の玉子焼きをお楽しみいただけます。',
+                    img:require('@/assets/images/tamago-pic.png'),
+                    alt:'玉子焼きイラスト',
+                    relatemenu:[
+                        {
+                            name:'玉子焼き（単品)',
+                            price:'350'
+                        },
+                        {
+                            name:'玉子焼き定食',
+                            price:'750',
+                        },
+                        {
+                            name:'焼き鳥玉子焼き定食',
+                            price:'1380'
+                        }
+                    ]
+                },
+                {
+                    name:'焼き鳥',
+                    price:'540',
+                    text:'長年に渡り、つけたし守り続けられた秘伝のたれをご堪能できます。栃木鳥の百日雛と呼ばれる雛鳥を使用しており、柔らかく味のしっかりとついた焼き鳥を是非ご賞味ください。',
+                    img:require('@/assets/images/yakitori-pic.png'),
+                    alt:'焼き鳥イラスト',
+                    relatemenu:[
+                        {
+                            name:'焼き鳥（単品）',
+                            price:'540'
+                        },
+                        {
+                            name:'焼き鳥定食',
+                            price:'940'
+                        },
+                        {
+                            name:'焼き鳥玉子焼き定食',
+                            price:'1380'
+                        }
+                    ]
+                },
+            ],
+            menus:[
+                {
+                    name:'だんご',
+                    price:'300',
+                },
+                {
+                    name:'焼き鳥',
+                    price:'540',
+                },
+                {
+                    name:'玉子焼',
+                    price:'350',
+                },
+                {
+                    name:'柚子みそおでん',
+                    price:'300',
+                },
+                {
+                    name:'ところ天',
+                    price:'300',
+                },
+                {
+                    name:'煮込みおでん',
+                    price:'500',
+                },
+                {
+                    name:'肉じゃが',
+                    price:'400',
+                },
+                {
+                    name:'もつ煮',
+                    price:'500',
+                },
+                {
+                    name:'焼き鳥定食',
+                    price:'940',
+                },
+                {
+                    name:'鳥めし',
+                    price:'940',
+                },
+                {
+                    name:'玉子焼定食',
+                    price:'750',
+                },
+                {
+                    name:'焼き鳥玉子焼定食',
+                    price:'1380',
+                },
+                {
+                    name:'カレーライス',
+                    price:'600',
+                },
+                {
+                    name:'山菜そば',
+                    price:'500',
+                },
+                {
+                    name:'山菜うどん',
+                    price:'500',
+                },
+                {
+                    name:'なめこそば',
+                    price:'500',
+                },
+                {
+                    name:'なめこうどん',
+                    price:'500'
+                }
+            ],
+            drinks:[
+                {
+                    name:'ビール',
+                    price:'630'
+                },
+                {
+                    name:'レモンハイ',
+                    price:'420',
+                },
+                {
+                    name:'コーラ',
+                    price:'210',
+                },
+                {
+                    name:'ジュース各種',
+                    price:'210円'
+                },
+                {
+                    name:'お酒各種',
+                    price:'420円'
+                }
             ]
         }
     }
 }
 </script>
+<style scoped lang="scss">
+@import '@/assets/variables.scss';
+.menu{
+    padding:100px 0;
+    margin:0 auto;
+    td{
+        padding:10px 80px 10px 0;
+        font-size:1.2rem;
+        @include sp(){
+            width:100%;
+            padding:10px 0;
+        }
+    }
+    th{
+        font-size:1.5rem;
+    }
+    td,th{
+        white-space:nowrap;
+        
+    }
+    tr{
+        border-bottom:1px solid brown;
+    }
+}
+</style>
---
+++ src/components/Parts/menudetails.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="about-wrapper">
+        <div class="wrapper-60">
+            <div class="vfor" v-for="main in this.mains" :key="main.name">
+                <div class="flex-wrapper">
+                    <h2  class="title" v-text="main.name"></h2>
+                    <img :src="main.img" :alt="main.alt">
+                </div>
+                <div class="center-wrapper ">
+                    <div class="about-container ">
+                        <div class="about-text ">
+                            <p v-text="main.text"></p>
+                        </div>
+                    </div>  
+                </div>
+                <div class="sub-menu" v-if="main.relatemenu">
+                    <p class="sub-title">料理を使ったメニュー</p>
+                    <div class="image-wrapper columns">
+                        <div class="image column is-3" v-for="relatemenu in main.relatemenu" :key="relatemenu.name">
+                            <p v-text="relatemenu.name"></p>
+                            <p>{{relatemenu.price}}&nbsp;円</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:[
+        'mains',
+    ]
+}
+</script>
+<style scoped lang="scss">
+@import '@/assets/variables.scss';
+.vfor{
+    margin:100px auto;
+    @include sp(){
+        margin:100px auto;
+    }
+}
+
+.wrapper-60{
+    width:60%;
+    margin:50px auto;
+    @include md(){
+        width:80%;
+    }
+    @include sp(){
+        width:90%;
+        margin:10px auto;
+    }
+}
+.about-container{
+    @include md(){
+        min-width:400px;
+    }
+    @include lg(){
+        min-width:400px;
+    }
+}
+.about-text{
+    p{
+        font-weight:bold;
+        font-size:1.2rem;
+    }
+}
+.image{
+    @include sp(){
+        margin-top:20px;
+        padding:5px;
+    }
+    img{
+        min-width:250px;
+        max-width:280px;
+        @include sp(){
+            width:150px;
+            margin:auto;
+        }
+    }
+    p{
+        font-weight:bold;
+        font-size:1.2rem;
+        text-align:left;
+        white-space:nowrap;
+        @include sp(){
+            text-align:center;
+            margin:10px 0;
+        }
+    }
+}
+.title{
+    width:100%;
+    font-weight:bold;
+    font-size:2rem;
+    border-bottom:1px solid rgba(0,0,0,0.3);
+    box-shadow:0 5px 10px -10px black;
+}
+.center-wrapper{
+    justify-content:space-between;
+}
+.sub-menu{
+    padding:50px 0;
+    @include sp(){
+        padding:50px 0;
+    }
+    .sub-title{
+        border-bottom:1px solid brown;
+        font-size:1.2rem;
+        @include sp(){
+            text-align:center;
+        }
+    }
+}
+.flex-wrapper{
+    position:relative;
+    h2{
+        @include sp(){
+            text-align:center;
+        }
+    }
+    img{
+        position:absolute;
+        top:30%;
+        left:25%;
+        transform:translate(-50%,-50%);
+        @include sp(){
+            left:20%;
+            width:25%;
+        }
+    }
+}
+</style>
