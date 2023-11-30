c776f749a66b7fad6bc43b9ec0f2c26101d44788
nakamura.hiroki <nakamura2465@gmail.com>
2023-11-30 20:03:27 +0900

    add: tailwindcssを導入した

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Buefy from 'buefy'
 import vueScrollTo from 'vue-scrollto'
 import 'buefy/dist/buefy.css'
+import '@/assets/tailwind.css'
 import {routes} from './routes'
 import App from './App.vue'
 import Topheader from './components/Parts/Header.vue'
@@ -11,6 +12,7 @@ import MainPage from './components/MainPage.vue'
 //GlobalなCSSの設定
 import'./assets/globalcss.scss'
 import '../node_modules/bulma/bulma.sass';
+
 Vue.config.productionTip = false
 
 
